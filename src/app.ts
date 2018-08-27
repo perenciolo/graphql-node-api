9226c474647b1ce7fc39c72692c2a37996def01f
Gustavo Henrique de Lima Perenciolo <991201@pucminas.br>
2018-08-27 17:50:12 -0300

    Adding post and user resolvers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
+
+import db from './models';
 import schema from './graphql/schema';
 
 class App {
@@ -13,10 +15,18 @@ class App {
   private middleware(): void {
     this.express.use(
       '/graphql',
-      graphqlHTTP({
+
+      (req, res, next) => {
+        req['context'] = {};
+        req['context'].db = db;
+        next();
+      },
+      
+      graphqlHTTP(req => ({
         schema: schema,
-        graphiql: process.env.NODE_ENV.trim() === 'development'
-      })
+        graphiql: process.env.NODE_ENV.trim() === 'development',
+        context: req['context']
+      }))
     );
   }
 }
